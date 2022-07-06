4128ce5e7da8e544c83685cf7f78e2adaebacda6
juningwei <289704828@qq.com>
2022-07-06 21:15:39 +0800

    封装网络请求

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,16 @@
   </div>
 </template>
 
+<script>
+import { registerAPI } from '@/api'
+export default {
+  async created () {
+    const res = await registerAPI()
+    console.log(res)
+  }
+}
+</script>
+
 <style lang="less" scoped>
 
 </style>
