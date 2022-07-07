7c5b1dea07dc2135bb90788802cb1dac9a96b36a
juningwei <289704828@qq.com>
2022-07-07 09:10:56 +0800

    表单标签和变量准备

--- src/views/register/index.vue
+++ src/views/register/index.vue
@@ -6,13 +6,37 @@
      <!-- 标题的盒子 -->
      <div class="title-box"></div>
      <!-- 注册的表单区域 -->
+     <el-form ref="form" :model="form">
+      <el-form-item>
+        <el-input placeholder="请输入用户名" v-model="form.username"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-input placeholder="请输入密码" v-model="form.password"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-input placeholder="请再次确认密码" v-model="form.repassword"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button class="btn-reg" type="primary" @click="registerFn">注册</el-button>
+        <el-link type="info">去登录</el-link>
+      </el-form-item>
+     </el-form>
    </div>
  </div>
 </template>
 
 <script>
 export default {
-  name: 'my-register'
+  name: 'my-register',
+  data () {
+    return {
+      form: {
+        username: '',
+        password: '',
+        repassword: ''
+      }
+    }
+  }
 }
 </script>
 
