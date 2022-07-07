8c7b67c3e09a6dbaa64639f773b3b46e61b8e978
juningwei <289704828@qq.com>
2022-07-07 09:31:39 +0800

    表单校验

--- src/views/register/index.vue
+++ src/views/register/index.vue
@@ -6,14 +6,14 @@
      <!-- 标题的盒子 -->
      <div class="title-box"></div>
      <!-- 注册的表单区域 -->
-     <el-form ref="form" :model="form">
-      <el-form-item>
+     <el-form ref="form" :model="form" :rules="rulesObj">
+      <el-form-item prop="username">
         <el-input placeholder="请输入用户名" v-model="form.username"></el-input>
       </el-form-item>
-      <el-form-item>
+      <el-form-item prop="password">
         <el-input placeholder="请输入密码" v-model="form.password"></el-input>
       </el-form-item>
-      <el-form-item>
+      <el-form-item prop="repassword">
         <el-input placeholder="请再次确认密码" v-model="form.repassword"></el-input>
       </el-form-item>
       <el-form-item>
@@ -29,11 +29,43 @@
 export default {
   name: 'my-register',
   data () {
+    const samePwd = (rule, value, callback) => {
+      if (value !== this.regForm.password) {
+        // 如果验证失败，则调用 回调函数时，指定一个 Error 对象。
+        callback(new Error('两次输入的密码不一致!'))
+      } else {
+        // 如果验证成功，则直接调用 callback 回调函数即可。
+        callback()
+      }
+    }
     return {
       form: {
         username: '',
         password: '',
         repassword: ''
+      },
+      rulesObj: {
+        username: [
+          { required: true, message: '请输入用户名', trigger: 'blur' },
+          {
+            pattern: /^[a-zA-Z0-9]{1,10}$/,
+            message: '用户名必须是1-10的大小写字母数字',
+            trigger: 'blur'
+          }
+        ],
+        password: [
+          { required: true, message: '请输入密码', trigger: 'blur' },
+          {
+            pattern: /^\S{6,15}$/,
+            message: '密码必须是6-15的非空字符',
+            trigger: 'blur'
+          }
+        ],
+        repassword: [
+          { required: true, message: '请再次输入密码', trigger: 'blur' },
+          { pattern: /^\S{6,15}$/, message: '密码必须是6-15的非空字符', trigger: 'blur' },
+          { validator: samePwd, trigger: 'blur' }
+        ]
       }
     }
   }
