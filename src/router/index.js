1adec75b36c81e8154d6e49b6c5a16a9fe625b2e
juningwei <289704828@qq.com>
2022-07-08 10:43:34 +0800

    在路由守卫中获取用户信息

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-
+import store from '@/store'
 Vue.use(VueRouter)
 
 const routes = [
@@ -20,6 +20,14 @@ const routes = [
 
 const router = new VueRouter({
   routes
+})
+
+router.beforeEach((to, from, next) => {
+  const token = store.state.token
+  if (token) {
+    store.dispatch('getUserInfoActions')
+  }
+  next()
 })
 
 export default router
