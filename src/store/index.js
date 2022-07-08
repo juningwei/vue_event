0fddab54629e8be5d68d5a5feb01113bea77248c
juningwei <289704828@qq.com>
2022-07-08 10:08:36 +0800

    保存用户信息到vuex

--- src/store/index.js
+++ src/store/index.js
@@ -1,20 +1,30 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import createPersistedState from 'vuex-persistedstate'
+import { getUserInfoAPI } from '@/api'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-    token: ''
+    token: '',
+    userInfo: {}
   },
   getters: {
   },
   mutations: {
     updateToken (state, newToken) {
       state.token = newToken
+    },
+    updateUserInfo (state, val) {
+      state.userInfo = val
     }
   },
   actions: {
+    async getUserInfoActions (store) {
+      const res = await getUserInfoAPI()
+      console.log(res)
+      store.commit('updateUserInfo', res.data.data)
+    }
   },
   modules: {
   },
